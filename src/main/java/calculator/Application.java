b1a18261264d65de76592ca784fb528dd825a649
alskung1101 <alskung1101@naver.com>
2025-10-20 21:07:07 +0900

    fix: Application.java 최종 로직 수정
    이전 커밋 이후 발견된 Application.java의 문제를 수정함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
-        // [최종] try-catch 블록이 없어야 합니다.
-        // 예외가 발생하면 main 밖으로 던져져야 테스트가 성공합니다.
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add(input);
 
