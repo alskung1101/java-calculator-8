bc47f293b6b781bedf7c009e6cec09e4f9eb49c6
alskung1101 <alskung1101@naver.com>
2025-10-20 21:05:28 +0900

    feat:Application 실행 및 예외 처리 구현
    Application.main에서 예외를 그대로 throw하도록 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,18 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        // [최종] try-catch 블록이 없어야 합니다.
+        // 예외가 발생하면 main 밖으로 던져져야 테스트가 성공합니다.
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add(input);
+
+        // 성공한 경우에만 결과 출력
+        System.out.println("결과 : " + result);
     }
 }
